dbb44b8274893f66d4f7506e9f87850e3f89a986
ATmilkyway <natnaelasegd0507@gmail.com>
2026-01-19 13:08:05 +0300

    feat(server): setup server

---
+++ server/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv/config";
+import cookieParser from "cookie-parser";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    // origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
+app.get("/", (req, res) => {
+  res.status(200).json("API is working");
+});
+
+try {
+  app.listen(PORT, () => {
+    console.log(`Server 🟢 http://localhost:${PORT}`);
+  });
+} catch (error) {
+  console.log("Server 🛑", error);
+}
