cd2906335df9671637d572a5a0556c92caccb910
ATmilkyway <natnaelasegd0507@gmail.com>
2026-01-19 13:55:42 +0300

    feat(server): db connection

---
+++ server/config/mongodb.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`DB 🟢 `);
+  } catch (error) {
+    console.log("DB 🛑", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- server/server.js
+++ server/server.js
@@ -2,10 +2,15 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv/config";
 import cookieParser from "cookie-parser";
+import connectDB from "./config/mongodb.js";
 
+// app
 const app = express();
-const PORT = process.env.PORT || 3000;
 
+// db
+connectDB();
+
+// middlewares
 app.use(express.json());
 app.use(cookieParser());
 app.use(
@@ -15,10 +20,13 @@ app.use(
   }),
 );
 
+// routes
 app.get("/", (req, res) => {
   res.status(200).json("API is working");
 });
 
+// start server
+const PORT = process.env.PORT || 3000;
 try {
   app.listen(PORT, () => {
     console.log(`Server 🟢 http://localhost:${PORT}`);
