20ac1fe1956ea2754bb72dfdef22bb6c82ca7eb9
ATmilkyway <natnaelasegd0507@gmail.com>
2026-01-20 23:30:43 +0300

    feat(server): user login

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -2,6 +2,7 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import userModle from "../models/userModel";
 
+// User registration
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -27,7 +28,7 @@ export const register = async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     // Create a new user
-    const newUser = userModle.create({
+    const newUser = await userModle.create({
       name: name,
       email: email,
       password: hashedPassword,
@@ -60,6 +61,66 @@ export const register = async (req, res) => {
         id: newUser._id,
         name: newUser.name,
         email: newUser.email,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+// User login
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  // Check for missing details
+  if (!email || !password) {
+    return res.status(400).json({
+      success: false,
+      message: "Email and password are required.",
+    });
+  }
+
+  try {
+    // Check if user exist
+    const userExist = await userModle.findOne({ email });
+
+    if (!userExist)
+      return res.status(401).json({
+        success: false,
+        message: "Invalid credentials.",
+      });
+
+    // Check if password is correct
+    const isPasswordValid = await bcrypt.compare(password, userExist.password);
+
+    if (!isPasswordValid)
+      return res.status(401).json({
+        success: false,
+        message: "Invalid credentials.",
+      });
+
+    // Genrate token
+    const token = jwt.sign({ id: userExist._id }, process.env.JWT_SECRET, {
+      expiresIn: "1hr",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "strict" : "none",
+      maxAge: 7 * 24 * 60 ** 60 * 1000, // a week
+    });
+
+    // Respond with success
+    return res.status(200).json({
+      success: false,
+      message: "Login succssful",
+      user: {
+        id: userExist._id,
+        email: userExist.email,
       },
     });
   } catch (error) {
