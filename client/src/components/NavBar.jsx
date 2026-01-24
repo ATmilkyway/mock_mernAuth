111411712eb54d5deb245bf5fa0ea8d797a9b7ef
ATmilkyway <natnaelasegd0507@gmail.com>
2026-01-24 14:32:45 +0300

    ui(client): navbar

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import { assets } from '../assets/assets';
+const NavBar = () => {
+  return (
+    <div className="absolute top-0 flex w-full items-center justify-between p-4 sm:p-6 sm:px-24">
+      <img src={assets.logo} className="w-28 sm:w-32" />
+      <button className="flex cursor-pointer items-center gap-2 rounded-full border border-gray-500 px-6 py-2 text-gray-800 hover:bg-gray-100">
+        Login <img src={assets.arrow_icon} />
+      </button>
+    </div>
+  );
+};
+
+export default NavBar;
