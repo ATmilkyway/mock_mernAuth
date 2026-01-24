9b0424b0d9c34631b23c304ccad55dd09393c14c
ATmilkyway <natnaelasegd0507@gmail.com>
2026-01-24 11:53:00 +0300

    feat(server): schema hardening

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,40 +1,49 @@
 import mongoose from "mongoose";
 
-const userSchama = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const userSchama = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    verifyOtp: {
+      type: String,
+      default: "",
+      select: false,
+    },
+    verifyOtpExpiresAt: {
+      type: Number,
+      default: 0,
+    },
+    isAccountVerified: {
+      type: Boolean,
+      default: false,
+    },
+    resetOtp: {
+      type: String,
+      default: "",
+      select: false,
+    },
+    resetOtpExpireAt: {
+      type: Number,
+      default: 0,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
+  {
+    timestamps: true,
+    strict: true, // extra fields will be ignored
   },
-  password: {
-    type: String,
-    required: true,
-  },
-  verifyOtp: {
-    type: String,
-    default: "",
-  },
-  verifyOtpExpiresAt: {
-    type: Number,
-    default: 0,
-  },
-  isAccountVerified: {
-    type: Boolean,
-    default: false,
-  },
-  resetOtp: {
-    type: String,
-    default: "",
-  },
-  resetOtpExpireAt: {
-    type: Number,
-    default: 0,
-  },
-});
+);
 
 const userModel = mongoose.models.User || mongoose.model("User", userSchama);
 
