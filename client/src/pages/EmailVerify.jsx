785c833c9e4a9bcfcb0a01f7a26cb4938cdfb351
ATmilkyway <natnaelasegd0507@gmail.com>
2026-01-26 00:44:45 +0300

    ui(client): build email verify form

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import { useRef } from 'react';
+import { assets } from '../assets/assets';
 
 const EmailVerify = () => {
-  return <div><h1>Email Verify</h1></div>;
+  const inputRefs = useRef([]);
+
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-linear-to-br from-blue-200 to-purple-400">
+      <img
+        src={assets.logo}
+        className="absolute top-5 left-5 w-28 cursor-pointer sm:left-20 sm:w-32"
+        onClick={() => navigate('/')}
+      />
+      <form className="w-96 rounded-lg bg-slate-900 p-8 text-sm shadow-lg">
+        <h1 className="mb-4 text-center text-2xl font-semibold text-white">Email Verify OTP</h1>
+        <p className="mb-6 text-center text-indigo-300">
+          Enter the 6 digit code sent to your email.
+        </p>
+        <div className="mb-8 flex justify-between">
+          {Array(6)
+            .fill(0)
+            .map((_, index) => (
+              <input
+                type="text"
+                maxLength={1}
+                key={index}
+                required
+                className="h-12 w-12 rounded-md bg-[#333A5C] text-center text-xl text-white"
+                autoComplete="false"
+                ref={(e) => (inputRefs.current[index] = e)}
+                onInput={(e) => handleInput(e, index)}
+              />
+            ))}
+        </div>
+        <button className="w-full rounded-full bg-linear-to-r from-indigo-500 to-indigo-900 py-3 text-white">
+          Verify email
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default EmailVerify;
