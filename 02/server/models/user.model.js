f6f284b3d13894f4282dfdaed39afaa4e87d29f3
ATmilkyway <natnaelasegd0507@gmail.com>
2026-01-29 03:46:03 +0300

    feat(server): create user models

---
+++ 02/server/models/user.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    lastlogin: {
+      type: Date,
+      default: Date.now,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    resetPasswordToken: String,
+    resetPasswordExpiredAt: Date,
+    verificationToken: String,
+    verificationTokenExpiredAt: Date,
+  },
+  { timestamps: true },
+);
+
+export const User = mongoose.model("User", userSchema);
