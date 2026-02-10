56cc15c0afa23e658f58a6afe7c20f28154a6058
ATmilkyway <natnaelasegd0507@gmail.com>
2026-02-10 21:26:30 +0300

    fix(server): errorHandler middleware

--- 03/server/src/index.ts
+++ 03/server/src/index.ts
@@ -1,8 +1,11 @@
+import cookieParser from "cookie-parser";
 import cors from "cors";
+import "dotenv/config";
 import express from "express";
 import connectToDatabase from "./config/db.js";
 import { APP_ORIGIN, NODE_ENV, PORT } from "./constants/env.js";
-import cookieParser from "cookie-parser";
+import errorHandler from "./middleware/errorHandler.js";
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -13,13 +16,19 @@ app.use(
   }),
 );
 app.use(cookieParser());
-app.get("/health", (req, res) => {
-  return res.status(200).json({
-    success: true,
-    message: "Hello World",
-  });
+app.get("/health", async (req, res, next) => {
+  try {
+    // throw new Error("This is a test error.");
+    return res.status(200).json({
+      success: true,
+      message: "Hello World",
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
+app.use(errorHandler);
 // app.use(error )
 const startServer = async () => {
   try {
---
+++ 03/server/src/middleware/errorHandler.ts
@@ -0,0 +1,8 @@
+import type { ErrorRequestHandler } from "express";
+
+const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
+  console.log(`PATH: ${req.path}`, error);
+  return res.status(500).send("Internal server error.")
+};
+
+export default errorHandler
