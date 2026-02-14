291bbcfb9a6132ea3bc71e178a0ef8af626c5085
ATmilkyway <natnaelasegd0507@gmail.com>
2026-02-14 12:10:03 +0300

    feat(server): update error function

--- 03/server/src/middleware/errorHandler.ts
+++ 03/server/src/middleware/errorHandler.ts
@@ -13,14 +13,21 @@ const handleZodError = (res: Response, error: z.ZodError) => {
     errors,
   });
 };
+
+const handleAppError = (res: Response, error: AppError) => {
+  res.status(error.statusCode).json({
+    message:error.message,
+    errorCode:error.errorCode
+  })
+};
 const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
   console.log(`PATH: ${req.path}`, error);
   if (error instanceof z.ZodError) {
     return handleZodError(res, error);
   }
 
-  // if (error instanceof AppError) return handleZodError
-    return res.status(INTERNAL_SERVER_ERROR).send("Internal server error.");
+  if (error instanceof AppError) return handleAppError(res, error);
+  return res.status(INTERNAL_SERVER_ERROR).send("Internal server error.");
 };
 
 export default errorHandler;
