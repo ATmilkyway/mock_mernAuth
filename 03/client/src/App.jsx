e098de450cadd0563b8b34c36e4e7a203673dd04
ATmilkyway <natnaelasegd0507@gmail.com>
2026-02-20 17:06:31 +0300

    ui(client): login page

--- 03/client/src/App.jsx
+++ 03/client/src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router";
 
 import React from "react";
+import Login from "./pages/Login";
 
 export const Home = () => {
   return <div>Home</div>;
@@ -10,6 +11,7 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
