e5ae8e810eb0bfb3fbfe98c8fbaa8636945be2e5
ATmilkyway <natnaelasegd0507@gmail.com>
2026-02-20 16:05:47 +0300

    feat(client): rollback to chakra2

--- 03/client/src/main.jsx
+++ 03/client/src/main.jsx
@@ -5,14 +5,18 @@ import { BrowserRouter } from "react-router";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "./config/queryClient.js";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme/index.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <ChakraProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </ChakraProvider>
   </StrictMode>,
 );
