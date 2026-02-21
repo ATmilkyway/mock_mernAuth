24753b6b71c1b6824cf6e82229bac12bbd98b3eb
ATmilkyway <natnaelasegd0507@gmail.com>
2026-02-21 17:08:59 +0300

    feat(client): verify email

--- 03/client/src/App.jsx
+++ 03/client/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router";
 import React from "react";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import VerifyEmail from "./pages/VerifyEmail";
 
 export const Home = () => {
   return <div>Home</div>;
@@ -14,6 +15,8 @@ const App = () => {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/email/verify/:code" element={<VerifyEmail />} />
+      {/* work on the security so it can only work only on logged device or get the logged in user id */}
     </Routes>
   );
 };
