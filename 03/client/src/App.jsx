d2a91592119a88a52cafea7548f6af423b79f7e9
ATmilkyway <natnaelasegd0507@gmail.com>
2026-02-21 17:18:25 +0300

    feat(client): forgot password

--- 03/client/src/App.jsx
+++ 03/client/src/App.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import VerifyEmail from "./pages/VerifyEmail";
+import ForgotPassword from "./pages/ForgotPassword";
 
 export const Home = () => {
   return <div>Home</div>;
@@ -17,9 +18,9 @@ const App = () => {
       <Route path="/register" element={<Register />} />
       <Route path="/email/verify/:code" element={<VerifyEmail />} />
       {/* work on the security so it can only work only on logged device or get the logged in user id */}
+      <Route path="/password/forgot" element={<ForgotPassword />} />
     </Routes>
   );
 };
 
 export default App;
-  
