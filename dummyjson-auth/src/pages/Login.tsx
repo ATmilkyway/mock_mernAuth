cd477c60c6dcdb7aaa86f6bd02a5af5d4a8f102c
ATmilkyway <natnaelasegd0507@gmail.com>
2026-02-25 18:42:40 +0300

    deps: install and setup shacdn lib

---
+++ dummyjson-auth/src/pages/Login.tsx
@@ -0,0 +1,9 @@
+import { Card } from "../components/ui/card";
+
+const Login = () => {
+  return (
+    <Card  className="w-full sm:max-w-md flex justify-center   items-center bg-red-400 h-full"></Card>
+  );
+};
+
+export default Login;
